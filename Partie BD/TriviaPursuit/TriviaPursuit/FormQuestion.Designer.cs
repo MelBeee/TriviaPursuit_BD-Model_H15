823109a64cf439b93336c61a6b90086444601430
MelBeee <mel.beee@deepteal.com>
2015-03-02 08:29:51 -0500

    gygkb

--- Partie BD/TriviaPursuit/TriviaPursuit/FormQuestion.Designer.cs
+++ Partie BD/TriviaPursuit/TriviaPursuit/FormQuestion.Designer.cs
@@ -28,98 +28,140 @@
       /// </summary>
       private void InitializeComponent()
       {
-         this.LB_Choix1 = new System.Windows.Forms.Label();
-         this.LB_Choix2 = new System.Windows.Forms.Label();
-         this.LB_Choix3 = new System.Windows.Forms.Label();
-         this.LB_Choix4 = new System.Windows.Forms.Label();
-         this.label1 = new System.Windows.Forms.Label();
-         this.SuspendLayout();
-         // 
-         // LB_Choix1
-         // 
-         this.LB_Choix1.BackColor = System.Drawing.Color.PaleGoldenrod;
-         this.LB_Choix1.Font = new System.Drawing.Font("Buxton Sketch", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-         this.LB_Choix1.Location = new System.Drawing.Point(58, 224);
-         this.LB_Choix1.Name = "LB_Choix1";
-         this.LB_Choix1.Size = new System.Drawing.Size(288, 38);
-         this.LB_Choix1.TabIndex = 1;
-         this.LB_Choix1.Text = "L\'oeuf";
-         this.LB_Choix1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-         this.LB_Choix1.Click += new System.EventHandler(this.LB_Choix1_Click);
-         this.LB_Choix1.MouseLeave += new System.EventHandler(this.LB_Choix1_MouseLeave);
-         this.LB_Choix1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LB_Choix1_MouseMove);
-         // 
-         // LB_Choix2
-         // 
-         this.LB_Choix2.BackColor = System.Drawing.Color.PaleGoldenrod;
-         this.LB_Choix2.Font = new System.Drawing.Font("Buxton Sketch", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-         this.LB_Choix2.Location = new System.Drawing.Point(58, 86);
-         this.LB_Choix2.Name = "LB_Choix2";
-         this.LB_Choix2.Size = new System.Drawing.Size(288, 38);
-         this.LB_Choix2.TabIndex = 2;
-         this.LB_Choix2.Text = "La Poule";
-         this.LB_Choix2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-         this.LB_Choix2.Click += new System.EventHandler(this.LB_Choix2_Click);
-         this.LB_Choix2.MouseLeave += new System.EventHandler(this.LB_Choix2_MouseLeave);
-         this.LB_Choix2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LB_Choix2_MouseMove);
-         // 
-         // LB_Choix3
-         // 
-         this.LB_Choix3.BackColor = System.Drawing.Color.PaleGoldenrod;
-         this.LB_Choix3.Font = new System.Drawing.Font("Buxton Sketch", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-         this.LB_Choix3.Location = new System.Drawing.Point(58, 132);
-         this.LB_Choix3.Name = "LB_Choix3";
-         this.LB_Choix3.Size = new System.Drawing.Size(288, 38);
-         this.LB_Choix3.TabIndex = 3;
-         this.LB_Choix3.Text = "La question ne sera jamais jamais répondu";
-         this.LB_Choix3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-         this.LB_Choix3.Click += new System.EventHandler(this.LB_Choix3_Click);
-         this.LB_Choix3.MouseLeave += new System.EventHandler(this.LB_Choix3_MouseLeave);
-         this.LB_Choix3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LB_Choix3_MouseMove);
-         // 
-         // LB_Choix4
-         // 
-         this.LB_Choix4.BackColor = System.Drawing.Color.PaleGoldenrod;
-         this.LB_Choix4.Font = new System.Drawing.Font("Buxton Sketch", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-         this.LB_Choix4.Location = new System.Drawing.Point(58, 178);
-         this.LB_Choix4.Name = "LB_Choix4";
-         this.LB_Choix4.Size = new System.Drawing.Size(288, 38);
-         this.LB_Choix4.TabIndex = 4;
-         this.LB_Choix4.Text = "Mystère de la vie";
-         this.LB_Choix4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-         this.LB_Choix4.Click += new System.EventHandler(this.LB_Choix4_Click);
-         this.LB_Choix4.MouseLeave += new System.EventHandler(this.LB_Choix4_MouseLeave);
-         this.LB_Choix4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LB_Choix4_MouseMove);
-         // 
-         // label1
-         // 
-         this.label1.BackColor = System.Drawing.Color.PaleGoldenrod;
-         this.label1.Font = new System.Drawing.Font("Buxton Sketch", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-         this.label1.Location = new System.Drawing.Point(12, 9);
-         this.label1.Name = "label1";
-         this.label1.Size = new System.Drawing.Size(380, 69);
-         this.label1.TabIndex = 0;
-         this.label1.Text = "Est-ce que la poule ou l\'oeuf en premier ? ";
-         this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-         // 
-         // FormQuestion
-         // 
-         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(226)))), ((int)(((byte)(69)))));
-         this.ClientSize = new System.Drawing.Size(404, 273);
-         this.Controls.Add(this.label1);
-         this.Controls.Add(this.LB_Choix4);
-         this.Controls.Add(this.LB_Choix3);
-         this.Controls.Add(this.LB_Choix2);
-         this.Controls.Add(this.LB_Choix1);
-         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-         this.MaximizeBox = false;
-         this.MinimizeBox = false;
-         this.Name = "FormQuestion";
-         this.Text = "FormQuestion";
-         this.Load += new System.EventHandler(this.FormQuestion_Load);
-         this.ResumeLayout(false);
+            this.LB_Choix1 = new System.Windows.Forms.Label();
+            this.LB_Choix2 = new System.Windows.Forms.Label();
+            this.LB_Choix3 = new System.Windows.Forms.Label();
+            this.LB_Choix4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PN_MSGRep = new System.Windows.Forms.Panel();
+            this.LB_MSGRep = new System.Windows.Forms.Label();
+            this.BTN_Continuer = new System.Windows.Forms.Button();
+            this.PN_MSGRep.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LB_Choix1
+            // 
+            this.LB_Choix1.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.LB_Choix1.Font = new System.Drawing.Font("Buxton Sketch", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Choix1.Location = new System.Drawing.Point(77, 107);
+            this.LB_Choix1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LB_Choix1.Name = "LB_Choix1";
+            this.LB_Choix1.Size = new System.Drawing.Size(384, 47);
+            this.LB_Choix1.TabIndex = 1;
+            this.LB_Choix1.Text = "L\'oeuf";
+            this.LB_Choix1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LB_Choix1.Click += new System.EventHandler(this.LB_Choix1_Click);
+            this.LB_Choix1.MouseLeave += new System.EventHandler(this.LB_Choix1_MouseLeave);
+            this.LB_Choix1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LB_Choix1_MouseMove);
+            // 
+            // LB_Choix2
+            // 
+            this.LB_Choix2.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.LB_Choix2.Font = new System.Drawing.Font("Buxton Sketch", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Choix2.Location = new System.Drawing.Point(77, 164);
+            this.LB_Choix2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LB_Choix2.Name = "LB_Choix2";
+            this.LB_Choix2.Size = new System.Drawing.Size(384, 47);
+            this.LB_Choix2.TabIndex = 2;
+            this.LB_Choix2.Text = "La Poule";
+            this.LB_Choix2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LB_Choix2.Click += new System.EventHandler(this.LB_Choix2_Click);
+            this.LB_Choix2.MouseLeave += new System.EventHandler(this.LB_Choix2_MouseLeave);
+            this.LB_Choix2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LB_Choix2_MouseMove);
+            // 
+            // LB_Choix3
+            // 
+            this.LB_Choix3.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.LB_Choix3.Font = new System.Drawing.Font("Buxton Sketch", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Choix3.Location = new System.Drawing.Point(77, 220);
+            this.LB_Choix3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LB_Choix3.Name = "LB_Choix3";
+            this.LB_Choix3.Size = new System.Drawing.Size(384, 47);
+            this.LB_Choix3.TabIndex = 3;
+            this.LB_Choix3.Text = "La question ne sera jamais jamais répondu";
+            this.LB_Choix3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LB_Choix3.Click += new System.EventHandler(this.LB_Choix3_Click);
+            this.LB_Choix3.MouseLeave += new System.EventHandler(this.LB_Choix3_MouseLeave);
+            this.LB_Choix3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LB_Choix3_MouseMove);
+            // 
+            // LB_Choix4
+            // 
+            this.LB_Choix4.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.LB_Choix4.Font = new System.Drawing.Font("Buxton Sketch", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Choix4.Location = new System.Drawing.Point(77, 277);
+            this.LB_Choix4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LB_Choix4.Name = "LB_Choix4";
+            this.LB_Choix4.Size = new System.Drawing.Size(384, 47);
+            this.LB_Choix4.TabIndex = 4;
+            this.LB_Choix4.Text = "Mystère de la vie";
+            this.LB_Choix4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LB_Choix4.Click += new System.EventHandler(this.LB_Choix4_Click);
+            this.LB_Choix4.MouseLeave += new System.EventHandler(this.LB_Choix4_MouseLeave);
+            this.LB_Choix4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LB_Choix4_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.label1.Font = new System.Drawing.Font("Buxton Sketch", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(507, 85);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Est-ce que la poule ou l\'oeuf en premier ? ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PN_MSGRep
+            // 
+            this.PN_MSGRep.Controls.Add(this.BTN_Continuer);
+            this.PN_MSGRep.Controls.Add(this.LB_MSGRep);
+            this.PN_MSGRep.Location = new System.Drawing.Point(0, 0);
+            this.PN_MSGRep.Name = "PN_MSGRep";
+            this.PN_MSGRep.Size = new System.Drawing.Size(539, 336);
+            this.PN_MSGRep.TabIndex = 5;
+            this.PN_MSGRep.Visible = false;
+            // 
+            // LB_MSGRep
+            // 
+            this.LB_MSGRep.Font = new System.Drawing.Font("Buxton Sketch", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_MSGRep.Location = new System.Drawing.Point(12, 9);
+            this.LB_MSGRep.Name = "LB_MSGRep";
+            this.LB_MSGRep.Size = new System.Drawing.Size(515, 316);
+            this.LB_MSGRep.TabIndex = 0;
+            this.LB_MSGRep.Text = "Bonne réponse !";
+            this.LB_MSGRep.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BTN_Continuer
+            // 
+            this.BTN_Continuer.Location = new System.Drawing.Point(222, 295);
+            this.BTN_Continuer.Name = "BTN_Continuer";
+            this.BTN_Continuer.Size = new System.Drawing.Size(95, 26);
+            this.BTN_Continuer.TabIndex = 1;
+            this.BTN_Continuer.Text = "Continuer";
+            this.BTN_Continuer.UseVisualStyleBackColor = true;
+            this.BTN_Continuer.Click += new System.EventHandler(this.BTN_Continuer_Click);
+            // 
+            // FormQuestion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(226)))), ((int)(((byte)(69)))));
+            this.ClientSize = new System.Drawing.Size(539, 336);
+            this.Controls.Add(this.PN_MSGRep);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LB_Choix4);
+            this.Controls.Add(this.LB_Choix3);
+            this.Controls.Add(this.LB_Choix2);
+            this.Controls.Add(this.LB_Choix1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormQuestion";
+            this.Text = "FormQuestion";
+            this.Load += new System.EventHandler(this.FormQuestion_Load);
+            this.PN_MSGRep.ResumeLayout(false);
+            this.ResumeLayout(false);
 
       }
 
@@ -130,5 +172,8 @@
       private System.Windows.Forms.Label LB_Choix3;
       private System.Windows.Forms.Label LB_Choix4;
       private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.Panel PN_MSGRep;
+      private System.Windows.Forms.Label LB_MSGRep;
+      private System.Windows.Forms.Button BTN_Continuer;
    }
 }
--- Partie BD/TriviaPursuit/TriviaPursuit/FormQuestion.cs
+++ Partie BD/TriviaPursuit/TriviaPursuit/FormQuestion.cs
@@ -10,92 +10,117 @@ using System.Windows.Forms;
 
 namespace TriviaPursuit
 {
-   public partial class FormQuestion : Form
-   {
-      public FormQuestion()
-      {
-         InitializeComponent();
-      }
+    public partial class FormQuestion : Form
+    {
+        string Reponse;
 
-      private void LB_Choix1_MouseLeave(object sender, EventArgs e)
-      {
-         LB_Choix1.ForeColor = Color.Black;
-         LB_Choix1.BackColor = Color.PaleGoldenrod;
-         Cursor.Current = Cursors.Arrow;
-      }
+        public FormQuestion()
+        {
+            InitializeComponent();
+        }
 
-      private void LB_Choix1_MouseMove(object sender, MouseEventArgs e)
-      {
-         LB_Choix1.ForeColor = Color.PaleGoldenrod;
-         LB_Choix1.BackColor = Color.Black;
-         Cursor.Current = Cursors.Hand;
-      }
+        private void LB_Choix1_MouseLeave(object sender, EventArgs e)
+        {
+            LB_Choix1.ForeColor = Color.Black;
+            LB_Choix1.BackColor = Color.PaleGoldenrod;
+            Cursor.Current = Cursors.Arrow;
+        }
 
-      private void LB_Choix2_MouseLeave(object sender, EventArgs e)
-      {
-         LB_Choix2.ForeColor = Color.Black;
-         LB_Choix2.BackColor = Color.PaleGoldenrod;
-         Cursor.Current = Cursors.Arrow;
-      }
+        private void LB_Choix1_MouseMove(object sender, MouseEventArgs e)
+        {
+            LB_Choix1.ForeColor = Color.PaleGoldenrod;
+            LB_Choix1.BackColor = Color.Black;
+            Cursor.Current = Cursors.Hand;
+        }
 
-      private void LB_Choix2_MouseMove(object sender, MouseEventArgs e)
-      {
-         LB_Choix2.ForeColor = Color.PaleGoldenrod;
-         LB_Choix2.BackColor = Color.Black;
-         Cursor.Current = Cursors.Hand;
-      }
+        private void LB_Choix2_MouseLeave(object sender, EventArgs e)
+        {
+            LB_Choix2.ForeColor = Color.Black;
+            LB_Choix2.BackColor = Color.PaleGoldenrod;
+            Cursor.Current = Cursors.Arrow;
+        }
 
-      private void LB_Choix3_MouseLeave(object sender, EventArgs e)
-      {
-         LB_Choix3.ForeColor = Color.Black;
-         LB_Choix3.BackColor = Color.PaleGoldenrod;
-         Cursor.Current = Cursors.Arrow;
-      }
+        private void LB_Choix2_MouseMove(object sender, MouseEventArgs e)
+        {
+            LB_Choix2.ForeColor = Color.PaleGoldenrod;
+            LB_Choix2.BackColor = Color.Black;
+            Cursor.Current = Cursors.Hand;
+        }
 
-      private void LB_Choix3_MouseMove(object sender, MouseEventArgs e)
-      {
-         LB_Choix3.ForeColor = Color.PaleGoldenrod;
-         LB_Choix3.BackColor = Color.Black;
-         Cursor.Current = Cursors.Hand;
-      }
+        private void LB_Choix3_MouseLeave(object sender, EventArgs e)
+        {
+            LB_Choix3.ForeColor = Color.Black;
+            LB_Choix3.BackColor = Color.PaleGoldenrod;
+            Cursor.Current = Cursors.Arrow;
+        }
 
-      private void LB_Choix4_MouseLeave(object sender, EventArgs e)
-      {
-         LB_Choix4.ForeColor = Color.Black;
-         LB_Choix4.BackColor = Color.PaleGoldenrod;
-         Cursor.Current = Cursors.Arrow;
-      }
+        private void LB_Choix3_MouseMove(object sender, MouseEventArgs e)
+        {
+            LB_Choix3.ForeColor = Color.PaleGoldenrod;
+            LB_Choix3.BackColor = Color.Black;
+            Cursor.Current = Cursors.Hand;
+        }
 
-      private void LB_Choix4_MouseMove(object sender, MouseEventArgs e)
-      {
-         LB_Choix4.ForeColor = Color.PaleGoldenrod;
-         LB_Choix4.BackColor = Color.Black;
-         Cursor.Current = Cursors.Hand;
-      }
+        private void LB_Choix4_MouseLeave(object sender, EventArgs e)
+        {
+            LB_Choix4.ForeColor = Color.Black;
+            LB_Choix4.BackColor = Color.PaleGoldenrod;
+            Cursor.Current = Cursors.Arrow;
+        }
 
-      private void FormQuestion_Load(object sender, EventArgs e)
-      {
+        private void LB_Choix4_MouseMove(object sender, MouseEventArgs e)
+        {
+            LB_Choix4.ForeColor = Color.PaleGoldenrod;
+            LB_Choix4.BackColor = Color.Black;
+            Cursor.Current = Cursors.Hand;
+        }
 
-      }
+        private void FormQuestion_Load(object sender, EventArgs e)
+        {
 
-      private void LB_Choix2_Click(object sender, EventArgs e)
-      {
+        }
 
-      }
+        private void LB_Choix2_Click(object sender, EventArgs e)
+        {
+            Reponse = LB_Choix2.Text;
+            AnalyseReponse();
+        }
 
-      private void LB_Choix3_Click(object sender, EventArgs e)
-      {
+        private void LB_Choix3_Click(object sender, EventArgs e)
+        {
+            Reponse = LB_Choix3.Text;
+            AnalyseReponse();
+        }
 
-      }
+        private void LB_Choix4_Click(object sender, EventArgs e)
+        {
+            Reponse = LB_Choix4.Text;
+            AnalyseReponse();
+        }
 
-      private void LB_Choix4_Click(object sender, EventArgs e)
-      {
+        private void LB_Choix1_Click(object sender, EventArgs e)
+        {
+            Reponse = LB_Choix1.Text;
+            AnalyseReponse();
+        }
 
-      }
+        private void AnalyseReponse()
+        {
+            bool bonnerep = true;
+            PN_MSGRep.Visible = true; 
+            if(bonnerep)
+            {
+                LB_MSGRep.Text = "Bonne réponse !";
+            }
+            else
+            {
+                LB_MSGRep.Text = "Mauvaise réponse !";
+            }
+        }
 
-      private void LB_Choix1_Click(object sender, EventArgs e)
-      {
-
-      }
-   }
+        private void BTN_Continuer_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
 }
