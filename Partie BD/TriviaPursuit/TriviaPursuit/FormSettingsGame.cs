a3bc96b758b3aeaa10eceec1ea4f47304b68f5a6
CharlieLaplante <alexylaplante@hotmail.com>
2015-03-16 17:43:06 -0400

    das
    dasda

--- Partie BD/TriviaPursuit/TriviaPursuit/FormSettingsGame.cs
+++ Partie BD/TriviaPursuit/TriviaPursuit/FormSettingsGame.cs
@@ -11,162 +11,177 @@ using Oracle.DataAccess.Client;
 
 namespace TriviaPursuit
 {
-    public partial class FormSettingsGame : Form
-    {
-        // variable contenant la connection a la bd 
-        OracleConnection oraconn = new OracleConnection();
+   public partial class FormSettingsGame : Form
+   {
+      // variable contenant la connection a la bd 
+      OracleConnection oraconn = new OracleConnection();
 
-        public FormSettingsGame(OracleConnection oraconnPrincipale)
-        {
-            InitializeComponent();
-            this.StartPosition = FormStartPosition.CenterParent;
-            oraconn = oraconnPrincipale;
-        }
+      public FormSettingsGame(OracleConnection oraconnPrincipale)
+      {
+         InitializeComponent();
+         this.StartPosition = FormStartPosition.CenterParent;
+         oraconn = oraconnPrincipale;
+      }
 
-        private bool VerificationJoueur(string NomJoueur)
-        {
-            bool Existe = false;
-            string nombre;
-            try
-            {
-                OracleCommand VerificationJoueur = new OracleCommand("GESTIONJOUER", oraconn);
-                VerificationJoueur.CommandText = "GESTIONJOUER.RECHERCHEJOUEUR";
-                VerificationJoueur.CommandType = CommandType.StoredProcedure;
+      private bool VerificationJoueur(string NomJoueur)
+      {
+         bool Existe = false;
+         string nombre;
+         try
+         {
+            OracleCommand VerificationJoueur = new OracleCommand("GESTIONJOUER", oraconn);
+            VerificationJoueur.CommandText = "GESTIONJOUER.RECHERCHEJOUEUR";
+            VerificationJoueur.CommandType = CommandType.StoredProcedure;
 
-                OracleParameter OrapamNom = new OracleParameter("Nom", OracleDbType.Varchar2, 20);
-                OrapamNom.Direction = ParameterDirection.Input;
-                OrapamNom.Value = NomJoueur;
-                VerificationJoueur.Parameters.Add(OrapamNom);
+            OracleParameter OrapamNom = new OracleParameter("Nom", OracleDbType.Varchar2, 20);
+            OrapamNom.Direction = ParameterDirection.Input;
+            OrapamNom.Value = NomJoueur;
+            VerificationJoueur.Parameters.Add(OrapamNom);
 
-                OracleParameter JoueurExiste = new OracleParameter("Existe", OracleDbType.Int32);
-                JoueurExiste.Direction = ParameterDirection.Output;
-                VerificationJoueur.Parameters.Add(JoueurExiste);
+            OracleParameter JoueurExiste = new OracleParameter("Existe", OracleDbType.Int32);
+            JoueurExiste.Direction = ParameterDirection.Output;
+            VerificationJoueur.Parameters.Add(JoueurExiste);
 
-                VerificationJoueur.ExecuteNonQuery();
+            VerificationJoueur.ExecuteNonQuery();
 
-                nombre = JoueurExiste.Value.ToString();
+            nombre = JoueurExiste.Value.ToString();
 
-                if (nombre == "1")
-                    Existe = true;
-            }
-            catch (OracleException ex)
-            {
-                GestionErreur(ex);
+            if (nombre == "1")
+               Existe = true;
+         }
+         catch (OracleException ex)
+         {
+            GestionErreur(ex);
 
-            }
-            return Existe;
-        }
+         }
+         return Existe;
+      }
+      protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+      {
+         switch (keyData)
+         {
+            case Keys.Enter:
+               DebuterLeJeu();
+               break;
 
-        private int TesterVide(string Nom, int nombre)
-        {
-            if (Nom != "")
-                if (VerificationJoueur(Nom))
-                    nombre++;
-                else
-                {
-                    MessageBox.Show("Le nom " + Nom + " n'existe pas.");
-                    nombre = 0;
-                }
+         }
+         return base.ProcessCmdKey(ref msg, keyData);        
+      }
 
-
-            return nombre;
-        }
-
-        private void BTN_Start_Click(object sender, EventArgs e)
-        {
-            int nbre = 0;
-
-            nbre = TesterVide(TB_P1.Text, nbre);
-            nbre = TesterVide(TB_P2.Text, nbre);
-            nbre = TesterVide(TB_P3.Text, nbre);
-            nbre = TesterVide(TB_P4.Text, nbre);
-
-            if (nbre >= 2)
-            {
-                SetterJoueur(nbre);
-                FormJeu form = new FormJeu(oraconn, TB_P1.Text, TB_P2.Text, TB_P3.Text, TB_P4.Text);
-
-                this.Hide();
-                if (form.ShowDialog() == DialogResult.Abort)
-                   this.DialogResult = DialogResult.Abort;
-                else
-                   this.Close();
-                
-            }
+      private int TesterVide(string Nom, int nombre)
+      {
+         if (Nom != "")
+            if (VerificationJoueur(Nom))
+               nombre++;
             else
             {
-                MessageBox.Show("Il n'y a pas assez de joueurs pour jouer");
+               MessageBox.Show("Le nom " + Nom + " n'existe pas.");
+               nombre = 0;
             }
-        }
 
-        private void SetterJoueur(int nbre)
-        {
-            if(nbre == 2)
-            {
-                if (TB_P1.Text == "" && TB_P2.Text == "")
-                {
-                    TB_P1.Text = TB_P3.Text;
-                    TB_P2.Text = TB_P4.Text;
-                    TB_P3.Text = "";
-                    TB_P4.Text = "";
-                }
-                else if(TB_P1.Text == "" && TB_P3.Text != "")
-                {
-                    TB_P1.Text = TB_P3.Text;
-                    TB_P3.Text = "";
-                }
-                else if (TB_P1.Text == "" && TB_P4.Text != "")
-                {
-                    TB_P1.Text = TB_P4.Text;
-                    TB_P4.Text = "";
-                }      
-                else if(TB_P2.Text == "" && TB_P3.Text != "")
-                {
-                    TB_P2.Text = TB_P3.Text;
-                    TB_P3.Text = "";
-                }
-                else if (TB_P2.Text == "" && TB_P4.Text != "")
-                {
-                    TB_P2.Text = TB_P4.Text;
-                    TB_P4.Text = "";
-                }  
-            }
-            else if(nbre == 3)
-            {
-                if(TB_P1.Text == "")
-                {
-                    TB_P1.Text = TB_P4.Text;
-                    TB_P4.Text = "";
-                }
-                else if (TB_P2.Text == "")
-                {
-                    TB_P2.Text = TB_P4.Text;
-                    TB_P4.Text = "";
-                }
-                else if (TB_P3.Text == "")
-                {
-                    TB_P3.Text = TB_P4.Text;
-                    TB_P4.Text = "";
-                }
-            }
-        }
 
-        private void BTN_Cancel_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
+         return nombre;
+      }
 
-        //////////////////////////////////////////////////////////////////////////////////////////////
-        //    Gestion des erreurs 
-        //////////////////////////////////////////////////////////////////////////////////////////////
-        private void GestionErreur(OracleException ex)
-        {
-            FormErreur form = new FormErreur(ex);
+      private void BTN_Start_Click(object sender, EventArgs e)
+      {
+         DebuterLeJeu();
+      }
+      private void DebuterLeJeu()
+      {
+         int nbre = 0;
 
+         nbre = TesterVide(TB_P1.Text, nbre);
+         nbre = TesterVide(TB_P2.Text, nbre);
+         nbre = TesterVide(TB_P3.Text, nbre);
+         nbre = TesterVide(TB_P4.Text, nbre);
+
+         if (nbre >= 2)
+         {
+            SetterJoueur(nbre);
+            FormJeu form = new FormJeu(oraconn, TB_P1.Text, TB_P2.Text, TB_P3.Text, TB_P4.Text);
+
+            this.Hide();
             if (form.ShowDialog() == DialogResult.Abort)
+               this.DialogResult = DialogResult.Abort;
+            else
+               this.Close();
+
+         }
+         else
+         {
+            MessageBox.Show("Il n'y a pas assez de joueurs pour jouer");
+         }
+      }
+
+      private void SetterJoueur(int nbre)
+      {
+         if (nbre == 2)
+         {
+            if (TB_P1.Text == "" && TB_P2.Text == "")
             {
-                this.DialogResult = DialogResult.Abort;
+               TB_P1.Text = TB_P3.Text;
+               TB_P2.Text = TB_P4.Text;
+               TB_P3.Text = "";
+               TB_P4.Text = "";
             }
-        }
-    }
+            else if (TB_P1.Text == "" && TB_P3.Text != "")
+            {
+               TB_P1.Text = TB_P3.Text;
+               TB_P3.Text = "";
+            }
+            else if (TB_P1.Text == "" && TB_P4.Text != "")
+            {
+               TB_P1.Text = TB_P4.Text;
+               TB_P4.Text = "";
+            }
+            else if (TB_P2.Text == "" && TB_P3.Text != "")
+            {
+               TB_P2.Text = TB_P3.Text;
+               TB_P3.Text = "";
+            }
+            else if (TB_P2.Text == "" && TB_P4.Text != "")
+            {
+               TB_P2.Text = TB_P4.Text;
+               TB_P4.Text = "";
+            }
+         }
+         else if (nbre == 3)
+         {
+            if (TB_P1.Text == "")
+            {
+               TB_P1.Text = TB_P4.Text;
+               TB_P4.Text = "";
+            }
+            else if (TB_P2.Text == "")
+            {
+               TB_P2.Text = TB_P4.Text;
+               TB_P4.Text = "";
+            }
+            else if (TB_P3.Text == "")
+            {
+               TB_P3.Text = TB_P4.Text;
+               TB_P4.Text = "";
+            }
+         }
+      }
+
+      private void BTN_Cancel_Click(object sender, EventArgs e)
+      {
+         this.Close();
+      }
+
+      //////////////////////////////////////////////////////////////////////////////////////////////
+      //    Gestion des erreurs 
+      //////////////////////////////////////////////////////////////////////////////////////////////
+      private void GestionErreur(OracleException ex)
+      {
+         FormErreur form = new FormErreur(ex);
+
+         if (form.ShowDialog() == DialogResult.Abort)
+         {
+            this.DialogResult = DialogResult.Abort;
+         }
+      }
+   }
 }
